2b50b25fd12a3aaf2ae4a718046e6f79ac13d85e
Milad Dehghan <milawddehghan@gmail.com>
2020-11-01 22:49:16 +0330

    service provider

--- src/SweetAuthServiceProvider.php
+++ src/SweetAuthServiceProvider.php
@@ -8,7 +8,14 @@ class SweetAuthServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-       dd('Run!');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sweetauth.php' => config_path('sweetauth.php'),
+                __DIR__ . '/../database/migrations/create_sweet_one_time_passwords.php.stub' => database_path('migrations/' . '2020_11_01_174513_create_sweet_one_time_passwords_table.php')
+            ]);
+        }
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 
     public function register()
