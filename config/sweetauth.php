d87ee55d750c86cd6b7010c92d88a9343e3295a7
Milad Dehghan <milawddehghan@gmail.com>
2020-11-01 22:50:21 +0330

    add some configs

--- config/sweetauth.php
+++ config/sweetauth.php
@@ -7,6 +7,10 @@ return [
         'phone_input_page_src' => 'test',
         'code_length' => 4,
         'sms_template' => 'verify',
-        'sms_failed_message' => 'Text Messages Are Not Delivered. Try Again'
+        'sms_failed_message' => 'Text Messages Are Not Delivered. Try Again',
+        'block_user_message' => 'Now Unavailable 4 you. Try Again!',
+        'delay_between_request' => 60,
+        'delay_count' => 1,
+        'delay_unblock' => 60
     ]
 ];
