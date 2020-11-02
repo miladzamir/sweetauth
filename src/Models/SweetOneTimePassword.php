cff2db6f6ef3f47bee4055e7652dbf8975213931
Milad Dehghan <milawddehghan@gmail.com>
2020-11-02 18:46:01 +0330

    add changes for migration

--- src/Models/SweetOneTimePassword.php
+++ src/Models/SweetOneTimePassword.php
@@ -9,10 +9,10 @@ class SweetOneTimePassword extends Model
     /**
      * @var string[]
      */
-    protected $fillable = ['phone', 'token', 'is_verify', 'is_block', 'request_times', 'last_send_status', 'last_send_at'];
+    protected $fillable = ['phone', 'token', 'is_verify', 'is_block', 'request_times', 'last_send_status', 'step', 'last_step_complete_at', 'last_send_at', 'r_token'];
 
     /**
      * @var string[]
      */
-    protected $dates = ['last_send_at'];
+    protected $dates = ['last_send_at' , 'last_step_complete_at'];
 }
