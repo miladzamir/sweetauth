aaf1abe139885ab2334a5e0f164cd5d3cb6dfd6c
Milad Dehghan <milawddehghan@gmail.com>
2020-11-06 02:34:21 +0330

    service provider

--- src/SweetAuthServiceProvider.php
+++ src/SweetAuthServiceProvider.php
@@ -5,6 +5,8 @@ namespace MiladZamir\SweetAuth;
 use MiladZamir\SweetAuth\Http\Middleware\IsReceiveAndStored;
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
+use MiladZamir\SweetAuth\Http\Middleware\IsRegisterStep;
+use MiladZamir\SweetAuth\Http\Middleware\IsVerify;
 
 class SweetAuthServiceProvider extends ServiceProvider
 {
@@ -13,13 +15,20 @@ class SweetAuthServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../config/sweetauth.php' => config_path('sweetauth.php'),
-                __DIR__ . '/../database/migrations/create_sweet_one_time_passwords.php.stub' => database_path('migrations/' . '2020_11_01_174513_create_sweet_one_time_passwords_table.php')
+                __DIR__ . '/../database/migrations/create_sweet_one_time_passwords.php.stub' => database_path('migrations/' . '2020_11_01_174513_create_sweet_one_time_passwords_table.php'),
             ]);
+
+            $this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/sweetauth'),
+            ], 'views');
         }
 
         $router = $this->app->make(Router::class);
+        $router->aliasMiddleware('is.register.step', IsRegisterStep::class);
         $router->aliasMiddleware('is.receive.and.stored', IsReceiveAndStored::class);
+        $router->aliasMiddleware('is.verify', IsVerify::class);
 
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'sweetauth');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 
