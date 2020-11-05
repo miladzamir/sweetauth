79e3b53555e0f358e2ef6d1c49ff87f9b15ad995
Milad Dehghan <milawddehghan@gmail.com>
2020-11-06 02:32:45 +0330

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'MiladZamir\SweetAuth\Http\Controllers'], function () {
+Route::group(['namespace' => 'MiladZamir\SweetAuth\Http\Controllers' , 'middleware' => 'web'], function () {
 
     Route::post('/ReceiveRequest', 'ReceiveRequestController@ReceiveRequest')->name('receive.request');
+
+    Route::post('/VerifyRequest', 'VerifyRequestController@VerifyRequest')->name('verify.request');
+
+    Route::post('/CreateUser', 'CreateUserController@CreateUser')->name('create.user');
 
 });
