7849be95d0a75b3b4eedc40c69e23558f8bfecbf
Milad Dehghan <milawddehghan@gmail.com>
2020-11-06 12:46:57 +0100

    change routes

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'MiladZamir\SweetAuth\Http\Controllers' , 'middleware' => 'web'], function () {
 
-    Route::post('/ReceiveRequest', 'ReceiveRequestController@ReceiveRequest')->name('receive.request');
-
-    Route::post('/VerifyRequest', 'VerifyRequestController@VerifyRequest')->name('verify.request');
-
-    Route::post('/CreateUser', 'CreateUserController@CreateUser')->name('create.user');
-
-
+Route::group(['namespace' => 'App\Http\Controllers\Auth' , 'middleware' => 'web'], function () {
     Auth::routes();
+    Route::post('/login', 'LoginController@login')->name('login');
+    Route::post('/logout', 'LoginController@logout')->name('logout');
+});
+
+Route::group(['namespace' => 'MiladZamir\SweetAuth\Http\Controllers' , 'middleware' => 'web'], function () {
+    Route::post('/ReceiveRequest', 'ReceiveRequestController@ReceiveRequest')->name('receive.request');
+    Route::post('/VerifyRequest', 'VerifyRequestController@VerifyRequest')->name('verify.request');
+    Route::post('/CreateUser', 'CreateUserController@CreateUser')->name('create.user');
     Route::get('/auth', 'ViewPageController@myView')->middleware('is.register.step')->name('auth');
     Route::get('/verify', 'ViewPageController@verify')->middleware('is.receive.and.stored')->name('verify');
     Route::get('/register', 'ViewPageController@register')->middleware('is.verify')->name('register');
     Route::get('/login', 'ViewPageController@login')->name('login');
-    Route::get('/home', 'ViewPageController@index')->name('home');
 });
+
