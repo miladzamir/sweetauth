06e5a0648c07eb8d8d247e7ede6f5760fb8c233a
Milad Dehghan <milawddehghan@gmail.com>
2020-11-11 02:52:36 +0330

    change name space to swauth

--- src/SweetAuthServiceProvider.php
+++ src/SweetAuthServiceProvider.php
@@ -14,7 +14,7 @@ class SweetAuthServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/sweetauth.php' => config_path('sweetauth.php'),
+                __DIR__ . '/../config/swauth.php' => config_path('swauth.php'),
                 __DIR__ . '/../database/migrations/create_sweet_one_time_passwords.php.stub' => database_path('migrations/' . '2020_11_01_174513_create_sweet_one_time_passwords_table.php'),
             ]);
 
