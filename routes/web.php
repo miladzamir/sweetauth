97bc4e1ed9301ba373c0b7f69f3103482070db93
Milad Dehghan <milawddehghan@gmail.com>
2020-11-11 02:53:18 +0330

    declear the get and post route

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::group(['namespace' => 'App\Http\Controllers\Auth', 'middleware' => 'web'], function () {
-    Route::post('/login', 'LoginController@login')->name('login');
-    Route::post('/logout', 'LoginController@logout')->name('logout');
-});
-
+/*
 Route::group(['namespace' => 'MiladZamir\SweetAuth\Http\Controllers', 'middleware' => 'web'], function () {
     Route::post('/ReceiveRequest', 'ReceiveRequestController@ReceiveRequest')->name('receive.request');
     Route::post('/VerifyRequest', 'VerifyRequestController@VerifyRequest')->name('verify.request');
     Route::post('/CreateUser', 'CreateUserController@CreateUser')->name('create.user');
     Route::post('/UpdateUserPassword', 'UpdateUserPasswordController@UserPassword')->name('user.password');
 
-    Route::get('/auth', 'ViewPageController@myView')->middleware('is.register.step')->name('auth');
-    Route::get('/verify', 'ViewPageController@verify')->middleware('is.receive.and.stored')->name('verify');
-    Route::get('/register', 'ViewPageController@register')->middleware('is.verify')->name('register');
-    Route::get('/login', 'ViewPageController@login')->name('login');
-    Route::get('/forgotPassword', 'ViewPageController@forgotPassword')->name('forgot.password');
-    Route::get('/resetPassword', 'ViewPageController@resetPassword')->middleware('is.verify')->name('reset.password');
+       Route::get('/auth', 'ViewPageController@myView')->middleware('is.register.step')->name('auth');
+        Route::get('/verify', 'ViewPageController@verify')->middleware('is.receive.and.stored')->name('verify');
+        Route::get('/register', 'ViewPageController@register')->middleware('is.verify')->name('register');
+        Route::get('/login', 'ViewPageController@login')->name('login');
+        Route::get('/forgotPassword', 'ViewPageController@forgotPassword')->name('forgot.password');
+        Route::get('/resetPassword', 'ViewPageController@resetPassword')->middleware('is.verify')->name('reset.password');
 
 });
+*/
 
+
+Route::group(['namespace' => 'MiladZamir\SweetAuth\Http\Controllers\Handler', 'middleware' => 'web'], function () {
+    Route::get(config('swauth.viewRouteNames.step1.0'), 'ViewHandlerController@stepOneMethodZero')->name(config('swauth.viewRouteNames.step1.0'));
+    Route::get(config('swauth.viewRouteNames.step1.1'), 'ViewHandlerController@stepOneMethodOne')->name(config('swauth.viewRouteNames.step1.1'));
+    Route::get(config('swauth.viewRouteNames.step2.0'), 'ViewHandlerController@stepTwoMethodZero')->name(config('swauth.viewRouteNames.step2.0'));
+    Route::get(config('swauth.viewRouteNames.step2.1'), 'ViewHandlerController@stepTwoMethodOne')->name(config('swauth.viewRouteNames.step2.1'));
+    Route::get(config('swauth.viewRouteNames.step3.0'), 'ViewHandlerController@stepThreeMethodZero')->name(config('swauth.viewRouteNames.step3.0'));
+    Route::get(config('swauth.viewRouteNames.step3.1'), 'ViewHandlerController@stepThreeMethodOne')->name(config('swauth.viewRouteNames.step3.1'));
+    Route::get(config('swauth.viewRouteNames.step4.0'), 'ViewHandlerController@stepFourMethodZero')->name(config('swauth.viewRouteNames.step4.0'));
+});
+
+Route::group(['namespace' => 'MiladZamir\SweetAuth\Http\Controllers', 'middleware' => 'web'], function () {
+    Route::post(config('swauth.postRouteNames.step1.0'), 'StepOneController@ReceiveRequest')->name(config('swauth.postRouteNames.step1.0'));
+    Route::post(config('swauth.postRouteNames.step2.0'), 'StepTwoController@ReceiveRequest')->name(config('swauth.postRouteNames.step2.0'));
+    Route::post(config('swauth.postRouteNames.step3.0'), 'StepThreeController@ReceiveRequest')->name(config('swauth.postRouteNames.step3.0'));
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Auth', 'middleware' => 'web'], function () {
+    Route::post(config('swauth.postRouteNames.step4.0'), 'LoginController@login')->name(config('swauth.viewRouteNames.step4.0'));
+    Route::post(config('swauth.postRouteNames.step5.0'), 'LoginController@logout')->name(config('swauth.viewRouteNames.step5.0'));
+});
