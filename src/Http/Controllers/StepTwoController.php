b1bf06d0daab9cee13bb8995504c924eb7cd699b
Milad Dehghan <milawddehghan@gmail.com>
2020-11-13 08:42:45 +0330

    step 2 start

--- src/Http/Controllers/StepTwoController.php
+++ src/Http/Controllers/StepTwoController.php
@@ -4,8 +4,26 @@ namespace MiladZamir\SweetAuth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use MiladZamir\SweetAuth\Helper;
+use MiladZamir\SweetAuth\Models\SweetOneTimePassword;
 
 class StepTwoController extends Controller
 {
-    //
+    public function configure(Request $request)
+    {
+        $stepTwoInput = config('swauth.inputs.step2');
+
+        self::validationToken($request, $stepTwoInput);
+        $session = Helper::checkSession('step1.0','step1.1');
+
+        $phoneInformation = SweetOneTimePassword::where('phone', session($session))->first();
+        dd($phoneInformation);
+    }
+
+    private function validationToken($request, $input)
+    {
+        $request->validate([
+            $input => config('swauth.validations.step2'),
+        ]);
+    }
 }
