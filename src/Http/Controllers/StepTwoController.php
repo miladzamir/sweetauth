c8878552e1158f9d530f05dbdabd10723b32684b
Milad Dehghan <milawddehghan@gmail.com>
2020-11-13 20:36:10 +0330

    complete step 2 and redirect to step 3

--- src/Http/Controllers/StepTwoController.php
+++ src/Http/Controllers/StepTwoController.php
@@ -3,7 +3,9 @@
 namespace MiladZamir\SweetAuth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use MiladZamir\SweetAuth\Helper;
 use MiladZamir\SweetAuth\Models\SweetOneTimePassword;
 
@@ -14,10 +16,39 @@ class StepTwoController extends Controller
         $stepTwoInput = config('swauth.inputs.step2');
 
         self::validationToken($request, $stepTwoInput);
-        $session = Helper::checkSession('step1.0','step1.1');
+        $session = Helper::checkSession('step1.0', 'step1.1');
 
         $phoneInformation = SweetOneTimePassword::where('phone', session($session))->first();
-        dd($phoneInformation);
+
+        if (Carbon::now()->timestamp - $phoneInformation->lastSmsSendAt() > config('swauth.mainConfig.scopeRange')) {
+            session()->forget($session);
+            if ($session == 'step1.0'){
+                return redirect()->route(config('swauth.viewRouteNames.step1.0'))
+                    ->withErrors(config('swauth.mainConfig.messages.outOfScope'));
+            }elseif ($session == 'step1.1'){
+                return redirect()->route(config('swauth.viewRouteNames.step1.1'))
+                    ->withErrors(config('swauth.mainConfig.messages.outOfScope'));
+            }else{
+                abort(403);
+            }
+        }
+
+        if ($phoneInformation->token != $request->$stepTwoInput){
+            return back()->withErrors(config('swauth.mainConfig.messages.invalidVerify'));
+        }
+
+        session()->forget($session);
+
+        if ($session == 'step1.0'){
+            Session::put('step2.0' , $request->$stepTwoInput);
+            return redirect()->route(config('swauth.viewRouteNames.step3.0'));
+        } elseif ($session == 'step1.1'){
+            Session::put('step2.1' , $request->$stepTwoInput);
+            return redirect()->route(config('swauth.viewRouteNames.step3.1'));
+        } else
+            abort(403);
+
+
     }
 
     private function validationToken($request, $input)
