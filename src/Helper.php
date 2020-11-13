f128f52ccd24aecb92add80ef76ba62bb2bf2557
Milad Dehghan <milawddehghan@gmail.com>
2020-11-13 08:41:23 +0330

    add helper function

--- src/Helper.php
+++ src/Helper.php
@@ -13,4 +13,21 @@ class Helper
         $url = explode('/', url()->previous());
         return end($url);
     }
+
+    public static function getLastNowUrl($request)
+    {
+        $url = explode('/', $request->url());
+        return end($url);
+    }
+
+    public static function checkSession($session1, $session2)
+    {
+        if (session()->has($session1))
+            return $session1;
+        elseif (session()->has($session2))
+            return $session2;
+        else
+            return false;
+
+    }
 }
