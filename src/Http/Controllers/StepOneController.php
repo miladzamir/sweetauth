38d1770ac4aed973e6bff37d3b1ff7a81c991c56
Milad Dehghan <milawddehghan@gmail.com>
2020-11-13 08:05:57 +0330

    stepOne complete

--- src/Http/Controllers/StepOneController.php
+++ src/Http/Controllers/StepOneController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 use MiladZamir\Orange\Orange;
 use MiladZamir\SweetAuth\Helper;
 use MiladZamir\SweetAuth\Models\SweetOneTimePassword;
@@ -26,6 +27,17 @@ class StepOneController extends Controller
         $phoneInformation = SweetOneTimePassword::where(config('swauth.inputs.step1') ,$request->$stepOneInput)->first();
 
 
+        if ($phoneInformation != null){
+            if ($this->blockSystem($phoneInformation) != 'userAllowed'){
+                $error = $this->getRemainingTimeMessage($phoneInformation, 'unBanAt');
+                return back()->withErrors($error);
+            }
+            if (Carbon::now()->timestamp - $phoneInformation->lastSmsSendAt() < $this->getStringBetween(config('swauth.mainConfig.messages.nextRequestAt'), ":", ":")) {
+                $error = $this->getRemainingTimeMessage($phoneInformation, 'nextRequestAt');
+                return back()->withErrors($error);
+            }
+        }
+
         $randomDigitNumber = $this->createRandomDigitNumber();
 
         $sendVerificationSms = Orange::smsLookup($request->$stepOneInput, $randomDigitNumber, '', '', config('swauth.orange.template'))->status();
@@ -39,7 +51,17 @@ class StepOneController extends Controller
         else
             $this->UpdateOneTimePassword($phoneInformation, $stepOneInput, $request, $randomDigitNumber);
 
-        return 'oj';
+
+        if (Helper::getLastUrl() == config('swauth.viewRouteNames.step1.0')){
+            Session::put('step1.0' , $request->$stepOneInput);
+            return redirect()->route(config('swauth.viewRouteNames.step2.0'));
+        }
+        elseif (Helper::getLastUrl() == config('swauth.viewRouteNames.step1.1')){
+            Session::put('step1.1' , $request->$stepOneInput);
+            return redirect()->route(config('swauth.viewRouteNames.step2.1'));
+        }
+        else
+            abort(403);
 
     }
 
@@ -87,5 +109,43 @@ class StepOneController extends Controller
             'last_step_complete_at' => Carbon::now(),
             'last_sms_send_at' => Carbon::now()
         ]);
+    }
+
+    private function getStringBetween($str, $from, $to)
+    {
+        $sub = substr($str, strpos($str, $from) + strlen($from), strlen($str));
+        return substr($sub, 0, strpos($sub, $to));
+    }
+
+    private function getRemainingTimeMessage($phoneInformation, $state)
+    {
+        $remainingTime = intval($this->getStringBetween(config('swauth.mainConfig.messages')[$state], ":", ":"));
+        $remainingCoolDown = Carbon::now()->timestamp - $phoneInformation->lastSmsSendAt();
+        return str_replace(':' . $remainingTime . ':', $remainingTime - $remainingCoolDown, config('swauth.mainConfig.messages')[$state]);
+    }
+
+    private function blockSystem($phoneInformation)
+    {
+        if ($phoneInformation->is_block == false){
+            if (Carbon::now()->timestamp - $phoneInformation->lastSmsSendAt() < config('swauth.mainConfig.delayBetweenRequest') && $phoneInformation->request_times > config('swauth.mainConfig.delayAllowedRequest')) {
+                $phoneInformation->update([
+                    'is_block' => true,
+                    'request_times' => 1
+                ]);
+                return 'userLimited';
+            }else{
+                return 'userAllowed';
+            }
+        }else{
+            if (Carbon::now()->timestamp - $phoneInformation->lastSmsSendAt() > $this->getStringBetween(config('swauth.mainConfig.messages.unBanAt'), ":", ":")) {
+                $phoneInformation->update([
+                    'is_block' => false,
+                    'request_times' => 1
+                ]);
+                return 'userAllowed';
+            }else{
+                return 'userLimited';
+            }
+        }
     }
 }
