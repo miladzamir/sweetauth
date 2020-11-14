e5e180d6e089091918d24dfb15c445e8efb9bee8
Milad Dehghan <milawddehghan@gmail.com>
2020-11-14 22:30:52 +0330

    fix bug in accept range

--- src/Http/Controllers/StepTwoController.php
+++ src/Http/Controllers/StepTwoController.php
@@ -20,7 +20,7 @@ class StepTwoController extends Controller
 
         $phoneInformation = SweetOneTimePassword::where('phone', session($session))->first();
 
-        if (Carbon::now()->timestamp - $phoneInformation->lastSmsSendAt() > config('swauth.mainConfig.scopeRange')) {
+        if (Carbon::now()->timestamp - $phoneInformation->lasStepCompleteAt() > config('swauth.mainConfig.scopeRange')) {
             session()->forget($session);
             if ($session == 'step1.0'){
                 return redirect()->route(config('swauth.viewRouteNames.step1.0'))
